562311a6ffdc458324fbda8cd585ecc0edbd5e95
Mark Koslow <markkoslow@Marks-MacBook-Pro.local>
2016-09-04 17:14:11 -0700

    Make sure location services work

--- AirtableProject/ViewController.swift
+++ AirtableProject/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import Cartography
+import GooglePlaces
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    private var mapView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.mapWithFrame(view.frame, camera: camera)
+        mapView.myLocationEnabled = true
+        view.addSubview(mapView)
+        self.mapView = mapView
+        
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
