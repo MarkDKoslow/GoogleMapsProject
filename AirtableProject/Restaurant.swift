8316a4ec9d967b0a15fabd5fa8a27fa52c040008
Mark Koslow <markdkoslow@gmail.com>
2016-09-04 17:50:17 -0700

    Add simple Restaurant model

---
+++ AirtableProject/Restaurant.swift
@@ -0,0 +1,13 @@
+//
+//  Restaurant.swift
+//  AirtableProject
+//
+//  Created by Mark Koslow on 9/4/16.
+//  Copyright © 2016 Mark Koslow. All rights reserved.
+//
+
+struct Restaurant {
+    let address: String
+    let name: String
+    let cuisine: String
+}
