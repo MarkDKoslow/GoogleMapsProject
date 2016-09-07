c881a48646128f56d35bf330bf19ae2c58019821
Mark Koslow <markdkoslow@gmail.com>
2016-09-07 09:36:25 -0700

    Implement reload map to get coordinates for each restaurant, create method to place markers on the map.

--- AirtableProject/ViewController.swift
+++ AirtableProject/ViewController.swift
@@ -7,56 +7,74 @@
 //
 
 import UIKit
-import Cartography
 import GooglePlaces
 import GoogleMaps
 
 class ViewController: UIViewController {
     
-    private var mapView: UIView?
+    private var mapView: GMSMapView?
+    private var restaurants: [Restaurant]? {
+        didSet {
+            reloadMap()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6.0)
+        // Create a GMSCameraPosition over San Francisco
+        let camera = GMSCameraPosition.cameraWithLatitude(37.789745, longitude: -122.41057, zoom: 12.0)
         let mapView = GMSMapView.mapWithFrame(view.frame, camera: camera)
         mapView.myLocationEnabled = true
         view.addSubview(mapView)
         self.mapView = mapView
         
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        
-        let url = NSURL(string: "https://api.airtable.com/v0/appJ8AGNLk708812e/RestaurantsList?api_key=keyk2tbwEshOdGpfd")
-        let session = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { data, response, error in
-            guard error == nil else { print(error); return }
-            
-            if let data = data {
-                do {
-                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: [])
-                    print("Printing JSON")
-                    print(json)
-                } catch {
-                    print("Could not be parsed properly")
-                }
-            }
+        // Fetch restaurants
+        APIClient.fetchResultsForEndpoint(.Restaurants, completion: { json in
+            self.restaurants = Restaurant.decodeArray(json)
         })
-        
-        session.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
+    func reloadMap() {
+        guard let restaurants = self.restaurants else { return }
+        
+        for restaurant in restaurants {
+            // If restaurant already has coordinates, place marker and continue
+            if let location = restaurant.coordinates {
+                dispatch_async(dispatch_get_main_queue(),{ [weak self] in         // Must place marker on main thread
+                    self?.placeMarkerForRestaurant(restaurant, atLocation: location)
+                })
+                continue
+            }
+            
+            // If restaurant has no coordinates, make sure address is present
+            guard let address = restaurant.address else { print("Restaurant: \(restaurant.name) has no address and no coordinates"); return }
+            
+            // Request coordinates for address, place marker
+            guard let requestURL = APIClient.Request.CoordinatesForAddress(address).URL() else { print("NO URL for \(restaurant)"); return }
+            
+            APIClient.fetchResultsForURL(requestURL, completion: { json in
+                guard let location = Coordinate.decode(json) else { print("Coordinate not decoded properly"); return }
+                
+                dispatch_async(dispatch_get_main_queue(),{ [weak self] in
+                    self?.placeMarkerForRestaurant(restaurant, atLocation: location)
+                })
+            })
+        }
+    }
+    
+    func placeMarkerForRestaurant(restaurant: Restaurant, atLocation location: Coordinate) {
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        marker.title = restaurant.name
+        marker.snippet = restaurant.cuisine
+        marker.map = mapView
+        marker.appearAnimation = kGMSMarkerAnimationPop
+    }
 }
 
