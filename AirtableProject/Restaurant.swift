925beb4b8a9c5b8043359fe890f9171b6236a29b
Mark Koslow <markdkoslow@gmail.com>
2016-09-07 09:35:15 -0700

    Create Restaurant struct and implement Mapper, JSONArrayDecodable

--- AirtableProject/Restaurant.swift
+++ AirtableProject/Restaurant.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2016 Mark Koslow. All rights reserved.
 //
 
-struct Restaurant {
-    let address: String
+import Mapper
+
+struct Restaurant: Mappable {
     let name: String
     let cuisine: String
+    let address: String?
+    var coordinates: Coordinate?
+    
+    init(map: Mapper) throws {
+        try name = map.from("Name")
+        try cuisine = map.from("Notes")
+        address = map.optionalFrom("Address")
+        if let coordinatesString: String = map.optionalFrom("Address") {    // Used for addresses with coordinate values
+            coordinates = coordinatesString.mapToCoordinate()
+        }
+    }
+}
+
+extension Restaurant: JSONArrayDecodable {
+    static func decodeArray(json: [String: AnyObject]) -> [Restaurant]? {
+        guard let records = json["records"] as? [[String: AnyObject]] else { fatalError("No records key when decoding restaurants") }
+        
+        var restaurants: [Restaurant] = []
+        for record in records {
+            if let fields = record["fields"] as? [String: String], restaurant = Restaurant.from(fields) {
+                restaurants.append(restaurant)
+            }
+        }
+        
+        return restaurants
+    }
 }
