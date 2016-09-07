f2ffd2ace3d59e3b6acd7e83a828ee92b914351d
Mark Koslow <markdkoslow@gmail.com>
2016-09-07 09:33:58 -0700

    Add Coordinate struct and implement Mappable, JSONDecodable

--- AirtableProject/Coordinates.swift
+++ AirtableProject/Coordinates.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import Mapper
+
+struct Coordinate: Mappable {
+    let latitude: Double
+    let longitude: Double
+    
+    init(map: Mapper) throws {
+        try latitude = map.from("lat")
+        try longitude = map.from("lng")
+    }
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
+
+extension Coordinate: JSONDecodable {
+    static func decode(json: [String: AnyObject]) -> Coordinate? {
+        guard let results = json["results"] as? [[String: AnyObject]] else { fatalError("No 'records' key when decoding Coordinates") }
+        
+        for result in results {
+            if let fields = result["geometry"] as? [String: AnyObject],
+                location = fields["location"] as? [String: Double],
+                coordinate = Coordinate.from(location) {
+                    return coordinate
+                }
+        }
+        
+        return nil
+    }
+}
+
